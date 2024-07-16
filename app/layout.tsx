642d50283f8d053655d7eed951afa89ac0b13b61
sonnguyen1 <sonnguyen1@beeketing.net>
2024-07-16 23:34:44 +0700

    fix: fix show favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,20 @@ export const metadata = {
   title: "Kiwi | Personal Portfolio",
   description: "Personal portfolio of Kiwi",
   icons: {
-    icon: '/favicon.ico', // /public path
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/favicon.ico',
+        href: '/favicon.ico',
+        sizes: "48xx48"
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/favicon.ico',
+        href: '/favicon.ico',
+        sizes: "48xx48"
+      }
+    ]
   },
 };
 
@@ -25,7 +38,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body
+    <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
